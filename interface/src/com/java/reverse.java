c299bb07256dabcad4ce56c18b2a3eb48d3db4d9
Sony <Sony@Sony-PC>
2016-06-09 19:09:32 +0530

    hi

---
+++ interface/src/com/java/reverse.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class reverse 
+{
+	public static void main(String[] args)
+	{ int a[]={1,2,3,4,5};
+		for(int i=4;i>=0;i--)
+			
+		{
+			System.out.println(a[i]);
+		}
+			
+	}
+
+}
